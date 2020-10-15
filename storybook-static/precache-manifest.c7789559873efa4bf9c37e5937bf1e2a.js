9189e5257eb4497fc0be524913fa711b74fc7316
alejo akerfeld <alejoakerfeld@gmail.com>
2020-10-15 11:13:58 -0300

    taskbox UI

---
+++ storybook-static/precache-manifest.c7789559873efa4bf9c37e5937bf1e2a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b8005bb5f0f4b6a204b9e0b5cdaedde",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.ecd7249483ad0c50831b.bundle.js"
+  },
+  {
+    "url": "runtime~main.ecd7249483ad0c50831b.bundle.js"
+  },
+  {
+    "revision": "19550dbb2db20561b274",
+    "url": "static/css/main.b15adadd.chunk.css"
+  },
+  {
+    "url": "vendors~main.ecd7249483ad0c50831b.bundle.js"
+  },
+  {
+    "url": "vendors~main.ecd7249483ad0c50831b.bundle.js.LICENSE.txt"
+  }
+]);
